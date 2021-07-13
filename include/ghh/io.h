6db86cd3330221ba3115a6066fa834497328c827
garrison hinson-hasty <garrisonhh@pm.me>
2021-07-12 22:10:27 -0400

    io files

---
+++ include/ghh/io.h
@@ -0,0 +1,7 @@
+#ifndef GHH_IO_H
+#define GHH_IO_H
+
+// string must be freed
+char *load_text_file(const char *filename);
+
+#endif
---
+++ src/io.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// string must be freed
+char *load_text_file(const char *filename) {
+	FILE *file;
+	char *text;
+	size_t text_size, i;
+
+	if ((file = fopen(filename, "r")) == NULL)
+		ERROR("Could not open file: %s\n", filename);
+
+	// get file size
+	text_size = 0;
+	while (fgetc(file) != EOF)
+		++text_size;
+
+	// copy file data
+	rewind(file);
+	text = malloc((text_size + 1) * sizeof(*text));
+
+	for (i = 0; i < text_size; ++i)
+		text[i] = fgetc(file);
+
+	text[i] = 0;
+
+	fclose(file);
+
+	return text;
+}
