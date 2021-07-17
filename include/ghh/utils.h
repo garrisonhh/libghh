dffe4b65c78e10ee787372d26e023736b56d1153
garrisonhh <garrisonhh@pm.me>
2021-07-17 15:04:55 -0400

    M_SQRT2 def

--- include/ghh/utils.h
+++ include/ghh/utils.h
@@ -8,7 +8,11 @@
 #include <math.h>
 
 #ifndef M_PI
-#define M_PI (3.14159265358979323846)
+#define M_PI	3.14159265358979323846
+#endif
+
+#ifndef M_SQRT2
+#define M_SQRT2	1.4142135623730951
 #endif
 
 // for 32-bit float
