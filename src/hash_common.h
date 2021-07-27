40a74df24191a5b7399dde563bc1a4c4c1cdd217
garrisonhh <garrisonhh@pm.me>
2021-07-27 10:39:13 -0400

    make hash function public

--- src/hash_common.h
+++ src/hash_common.h
@@ -4,44 +4,14 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <ghh/hash.h>
 #define GHH_MEMCHECK_OVERRIDES
 #include <ghh/memcheck.h>
 #undef GHH_MEMCHECK_OVERRIDES
 
-// 64 bit
-#if INTPTR_MAX == INT64_MAX
-
-typedef uint64_t hash_t;
-#define FNV_PRIME 0x00000100000001b3
-#define FNV_BASIS 0xcbf29ce484222325
-
-#else
-
-// 32 bit
-typedef uint32_t hash_t;
-#define FNV_PRIME 0x01000193a
-#define FNV_BASIS 0x811c9dc5
-
-#endif
-
 // fnv-1a hash function (http://isthe.com/chongo/tech/comp/fnv/)
 static inline hash_t hash_key(int key_size, const void *key) {
-	hash_t hash = FNV_BASIS;
-	const uint8_t *bytes = key;
-
-	if (key_size < 0) {
-		while (*bytes) {
-			hash ^= *bytes++;
-			hash *= FNV_PRIME;
-		}
-	} else {
-		for (int i = 0; i < key_size; ++i) {
-			hash ^= bytes[i];
-			hash *= FNV_PRIME;
-		}
-	}
-
-	return hash;
+	return key_size < 0 ? hash(key, strlen(key)) : hash(key, key_size);
 }
 
 static inline const void *copy_key(int key_size, const void *key) {
