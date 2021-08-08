d79cda4a51290ecb3354da520b4d66b7c7588d3c
garrisonhh <garrisonhh@pm.me>
2021-08-08 14:27:23 -0400

    SANITY_CHECK macro

--- include/ghh/utils.h
+++ include/ghh/utils.h
@@ -15,6 +15,8 @@
 #define M_SQRT2	1.4142135623730951
 #endif
 
+#define EPSILON	0.000001
+
 // common macros
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
@@ -30,45 +32,54 @@
 #define FOR_CUBE(x, y, z, minv, maxv) for (z = minv; z < maxv; ++z) for (y = minv; y < maxv; ++y) for (x = minv; x < maxv; ++x)
 
 // bitfield ops
+// TODO use BIT_SET() and BIT_CLEAR() terminology?
 #define BIT_GET(bitfield, index) ((bitfield >> (index)) & 1)
-#define BIT_SET_TRUE(bitfield, index) {bitfield |= (0x1 << (index));}
-#define BIT_SET_FALSE(bitfield, index) {bitfield &= ~(0x1 << (index));}
+#define BIT_SET_TRUE(bitfield, index) do {bitfield |= (0x1 << (index));} while (0);
+#define BIT_SET_FALSE(bitfield, index) do {bitfield &= ~(0x1 << (index));} while (0);
 
-#define BIT_SET_COND(bitfield, index, cond) do {\
-	if (cond) \
-		BIT_SET_TRUE(bitfield, index) \
-	else \
-		BIT_SET_FALSE(bitfield, index) \
-} while (0)
+#define BIT_SET_COND(bitfield, index, cond)\
+	do {\
+		if (cond) \
+			BIT_SET_TRUE(bitfield, index) \
+		else \
+			BIT_SET_FALSE(bitfield, index) \
+	} while (0)
 
 void print_bits(const char *message, unsigned n, size_t bits);
 
 // swap
 // evil xor hack
 #define SWAP(a, b) (((a) ^ (b)) && ((b) ^= (a) ^= (b), (a) ^= (b)))
-#define SWAP_TMP(a, b, temp) do {\
-	temp = a;\
-	a = b;\
-	b = temp;\
-} while (0)
+#define SWAP_TMP(a, b, temp)\
+	do {\
+		temp = a;\
+		a = b;\
+		b = temp;\
+	} while (0)
 
 // swp is bool/unsigned/int
-#define BIT_SET_SWAP(bitfield, index1, index2, swp) do {\
-	swp = BIT_GET(bitfield, index1);\
-	BIT_SET_COND(bitfield, index1, BIT_GET(bitfield, index2));\
-	BIT_SET_COND(bitfield, index2, swp);\
-} while (0)
+#define BIT_SET_SWAP(bitfield, index1, index2, swp)\
+	do {\
+		swp = BIT_GET(bitfield, index1);\
+		BIT_SET_COND(bitfield, index1, BIT_GET(bitfield, index2));\
+		BIT_SET_COND(bitfield, index2, swp);\
+	} while (0)
 
 // errors
-#define ERROR(...) do {\
-	fprintf(stderr, "ERROR:%s:%d\n", __FILE__, __LINE__);\
-	fprintf(stderr, __VA_ARGS__);\
-	exit(-1);\
-} while (0)
+#define ERROR(...)\
+	do {\
+		fprintf(stderr, "ERROR:%s:%d\n", __FILE__, __LINE__);\
+		fprintf(stderr, __VA_ARGS__);\
+		exit(-1);\
+	} while (0)
+
+#ifdef DEBUG
+#define SANITY_CHECK(cond, ...) if (!(cond)) ERROR(__VA_ARGS__)
+#else
+#define SANITY_CHECK(...)
+#endif
 
 // float stuff
-#define EPSILON	0.000001
-
 #define fround(d) (floor(d + 0.5 + EPSILON))
 
 static inline bool fequals(double a, double b) {
