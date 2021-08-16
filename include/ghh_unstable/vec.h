9383debf634fb34e57cbf96a36b4fc5151c24168
garrisonhh <garrisonhh@pm.me>
2021-08-16 14:13:25 -0400

    fvec
    fat pointer version of vec with almost identical api :)

--- include/ghh_unstable/vec.h
+++ include/ghh_unstable/vec.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+// configuration is shared by vec and fvec
+
 #ifndef VEC_MIN_CAP
 #define VEC_MIN_CAP 4
 #endif
@@ -16,17 +18,22 @@ const unsigned VEC_ORDERED = 1 << 0;
 // chunks rather than by a factor of 2
 const unsigned VEC_LINEAR = 1 << 1;
 
+#define GHH_NUM_VEC_FLAGS 2
+
+// vec =========================================================================
+// dynamic array implementation using void *
+
 typedef struct ghh_vec {
     void **data;
     size_t size, cap, min_cap;
-    unsigned flags: 2;
+    unsigned flags: GHH_NUM_VEC_FLAGS;
 } vec_t;
 
-// exclusively for vec_init macro
+// used for vec_make macro
 struct ghh_vec_init_cfg {
     vec_t *vec;
     size_t init_cap;
-    unsigned flags: 2;
+    unsigned flags: GHH_NUM_VEC_FLAGS;
 };
 
 void vec_make_internal(struct ghh_vec_init_cfg);
@@ -45,11 +52,11 @@ void vec_remove(vec_t *, size_t idx);
 static inline void vec_clear(vec_t *vec) {
     if (vec->size) {
         vec_kill(vec);
-        vec_make(.vec = vec, .init_cap = vec->min_cap, .flags = vec->flags);
+        vec_make(vec, .init_cap = vec->min_cap, .flags = vec->flags);
     }
 }
 
-// macros
+// utilities
 #define vec_foreach(item, vec)\
     for (size_t vec_idx = 0;\
         (item = (vec)->data[vec_idx], vec_idx < (vec)->size);\
@@ -64,5 +71,72 @@ static inline void vec_clear(vec_t *vec) {
 
 #define vec_qsort(vec, compare)\
     qsort(vec->data, vec->size, sizeof(*vec->data), compare)
+
+// fvec ========================================================================
+// dynamic array implementation using fat pointers (fvec == fat vector) + macros
+
+// internal stuff
+struct ghh_fvec {
+    size_t size, cap, min_cap;
+    unsigned flags: GHH_NUM_VEC_FLAGS;
+};
+
+struct ghh_fvec_init_cfg {
+    size_t item_size, init_cap;
+    unsigned flags: GHH_NUM_VEC_FLAGS;
+    unsigned discard: 1;
+};
+
+void *fvec_make_internal(struct ghh_fvec_init_cfg);
+void *fvec_alloc_one_internal(size_t item_size, struct ghh_fvec *fv);
+void *fvec_free_one_internal(size_t item_size, struct ghh_fvec *fv);
+#define fvec_make2(ptr, ...)\
+    (ptr) = fvec_make_internal(\
+        (struct ghh_fvec_init_cfg){sizeof(*(ptr)), __VA_ARGS__});
+
+// make/kill
+#define fvec(type) type * // just to show programmer intent
+#define fvec_make(...) do { fvec_make2(__VA_ARGS__, .discard = 0) } while (0)
+#define fvec_kill(ptr) free(fvec_data(ptr))
+
+// getters
+#define fvec_data(ptr) ((struct ghh_fvec *)ptr - 1)
+#define fvec_size(ptr) (fvec_data(ptr)->size)
+#define fvec_cap(ptr) (fvec_data(ptr)->cap)
+#define fvec_min_cap(ptr) (fvec_data(ptr)->min_cap)
+#define fvec_flags(ptr) (fvec_data(ptr)->flags)
+
+// LIFO stack ops
+#define fvec_push(ptr, item)\
+    do {\
+        ptr = fvec_alloc_one_internal(sizeof(*ptr), fvec_data(ptr));\
+        ptr[fvec_size(ptr)++] = item;\
+    } while (0)
+#define fvec_pop(ptr, item) (\
+        ptr = fvec_free_one_internal(sizeof(*ptr), fvec_data(ptr)),\
+        ptr[--fvec_size(ptr)]\
+    )
+#define fvec_peek(ptr) ptr[fvec_size(ptr) - 1]
+
+// dynamic array ops
+// TODO
+
+// utilities
+#define fvec_foreach(item, ptr)\
+    for (size_t fvec_idx = 0;\
+        (item = ptr[fvec_idx], fvec_idx < fvec_size(ptr));\
+        ++fvec_idx)
+
+#define fvec_map(ptr, func)\
+    do {\
+        void *start_ptr = ptr;\
+        size_t ptr_size = fvec_size(ptr);\
+        while (ptr_size--)\
+            func(*ptr++);\
+        ptr = start_ptr;\
+    } while (0)
+
+#define fvec_qsort(ptr, compare)\
+    qsort(ptr, fvec_size(ptr), sizeof(*ptr), compare)
 
 #endif
--- src/unstable/vec.c
+++ src/unstable/vec.c
@@ -3,6 +3,8 @@
 #include <ghh_unstable/vec.h>
 #include <ghh_unstable/utils.h>
 
+// vec =========================================================================
+
 void vec_make_internal(struct ghh_vec_init_cfg cfg) {
     SANITY_CHECK(cfg.vec, "must pass a valid vec pointer to vec_init.\n");
 
@@ -17,11 +19,10 @@ void vec_make_internal(struct ghh_vec_init_cfg cfg) {
 
 static void alloc_one(vec_t *vec) {
     if (vec->size + 1 > vec->cap) {
-        vec->cap = vec->flags & VEC_LINEAR_GROWTH
+        vec->cap = vec->flags & VEC_LINEAR
             ? vec->cap + VEC_LINEAR_GROWTH
             : vec->cap * 2;
         vec->data = realloc(vec->data, vec->cap * sizeof(*vec->data));
-        printf("resizing to %zu\n", vec->cap);
     }
 }
 
@@ -31,7 +32,7 @@ static void free_one(vec_t *vec) {
     if (vec->cap > vec->min_cap) {
         bool resize = false;
 
-        if (vec->flags & VEC_LINEAR_GROWTH) {
+        if (vec->flags & VEC_LINEAR) {
             if ((resize = vec->size < vec->cap - VEC_LINEAR_GROWTH * 2))
                 vec->cap -= VEC_LINEAR_GROWTH;
         } else {
@@ -39,10 +40,8 @@ static void free_one(vec_t *vec) {
                 vec->cap /= 2;
         }
 
-        if (resize) {
+        if (resize)
             vec->data = realloc(vec->data, vec->cap * sizeof(*vec->data));
-            printf("resizing to %zu\n", vec->cap);
-        }
     }
 }
 
@@ -95,4 +94,55 @@ void vec_remove(vec_t *vec, size_t idx) {
 
     free_one(vec);
     --vec->size;
+}
+
+// fvec ========================================================================
+
+#define FVEC_BYTES(item_size, cap)\
+    (sizeof(struct ghh_fvec) + (item_size) * (cap))
+
+void *fvec_make_internal(struct ghh_fvec_init_cfg cfg) {
+    cfg.init_cap = MAX(cfg.init_cap, VEC_MIN_CAP);
+
+    struct ghh_fvec *fv;
+
+    fv = malloc(FVEC_BYTES(cfg.item_size, cfg.init_cap));
+    fv->size = 0;
+    fv->cap = fv->min_cap = cfg.init_cap;
+    fv->flags = cfg.flags;
+
+    return fv + 1;
+}
+
+void *fvec_alloc_one_internal(size_t item_size, struct ghh_fvec *fv) {
+    if (fv->size + 1 > fv->cap) {
+        fv->cap = fv->flags & VEC_LINEAR
+            ? fv->cap + VEC_LINEAR_GROWTH
+            : fv->cap * 2;
+        fv = realloc(fv, FVEC_BYTES(item_size, fv->cap));
+    }
+
+    return fv + 1;
+}
+
+void *fvec_free_one_internal(size_t item_size, struct ghh_fvec *fv) {
+    SANITY_CHECK(fv->size, "can't shrink fvec of zero size.\n");
+
+    if (fv->cap > fv->min_cap) {
+        bool resize = false;
+
+        // for the macro to work, the last element must always remain valid
+        if (fv->flags & VEC_LINEAR) {
+            if ((resize = fv->size < fv->cap - VEC_LINEAR_GROWTH * 2))
+                fv->cap -= VEC_LINEAR_GROWTH;
+        } else {
+            if ((resize = fv->size < fv->cap / 4))
+                fv->cap /= 2;
+        }
+
+        if (resize)
+            fv = realloc(fv, FVEC_BYTES(item_size, fv->cap));
+    }
+
+    return fv + 1;
 }
